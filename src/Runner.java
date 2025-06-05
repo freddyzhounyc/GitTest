94e46b5a93cd567a7c0ccbc5950e51c9ebcecab1
Freddy Zhou <freddyzhou06@gmail.com>
2025-06-04 21:21:49 -0400

    Add HashMap

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,7 @@ import java.util.*;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+
+        Map<Integer, Integer> newMap = new HashMap<>(); //subtype polymorphism
     }
 }
